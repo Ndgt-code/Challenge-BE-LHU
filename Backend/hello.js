18165485458bc403d9df9f7ab57ed84771fd8c1b
Trường Nguyễn <nguyendanggiangtruong@gmail.com>
2025-12-29 14:10:12 +0700

    Day 1
    Cài Node.js, Postman, chạy file JS đầu tiên, REPL

---
+++ Backend/hello.js
@@ -0,0 +1,16 @@
+// File JavaScript đầu tiên của bạn
+console.log("Hello Node.js!");
+
+// Các kiểu dữ liệu cơ bản
+const name = "Developer";
+const age = 25;
+const isLearning = true;
+
+console.log(`Xin chào, tôi là Nguyễn Đặng Giang Trường , ${age} tuổi: 22`);
+console.log(`Đang học Node.js: ${isLearning}`);
+
+// Sử dụng một số tính năng Node.js
+console.log("\n--- Thông tin hệ thống ---");
+console.log("Node.js version:", process.version);
+console.log("Platform:", process.platform);
+console.log("Current directory:", process.cwd());
